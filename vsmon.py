d230b300d79adc89d948e5a46ffcb29910a2df6a
garacio <garacio@gmail.com>
2013-07-19 17:05:16 +0400

    add new features

--- vsmon.py
+++ vsmon.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import sys
+import time
 import re
 from pysphere import *
 from optparse import OptionParser,OptionGroup
@@ -21,6 +22,14 @@ verbose=False
 
 # timeout
 timeout = 0
+
+metrics = [
+	'cpu.utilization',
+	'mem.usage',
+	'mem.swapused',
+	'mem.active',
+	'net.usage',
+	]
 
 # ----------------------------------------------------------------------
 
@@ -43,7 +52,7 @@ class bcolors:
 # ----------------------------------------------------------------------
 
 def getopts() :
-  global hostname,user,password,verbose
+  global hostname,user,password,verbose,summary,clean
   usage = "usage: %prog -H hostname -U username -P password [ -v ]\n\n" \
     "example: %prog -H my-shiny-new-vmware-server -U root -P fakepassword \n\n" \
     "or, verbosely:\n\n" \
@@ -60,6 +69,10 @@ def getopts() :
 
   group2.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, \
       help="print status messages to stdout (default is to be quiet)")
+  group2.add_option("-s", "--summary", action="store_true", dest="summary", default=True, \
+      help="print summary statistics for monitoring")
+  group2.add_option("-c", "--clean", action="store_true", dest="clean", default=False, \
+      help="Clean print")
 
   parser.add_option_group(group1)
   parser.add_option_group(group2)
@@ -69,30 +82,16 @@ def getopts() :
     print "no parameters specified\n"
     parser.print_help()
     sys.exit(-1)
-  # if first argument starts with 'https://' we have old-style parameters, so handle in old way
-  if re.match("https://",sys.argv[1]):
-    # check input arguments
-    if len(sys.argv) < 5:
-      print "too few parameters\n"
+
+  (options, args) = parser.parse_args()
+
+  # Making sure all mandatory options appeared.
+  mandatories = ['host', 'user', 'password']
+  for m in mandatories:
+    if not options.__dict__[m]:
+      print "mandatory parameter '--" + m + "' is missing\n"
       parser.print_help()
       sys.exit(-1)
-    if len(sys.argv) > 5 :
-      if sys.argv[5] == "verbose" :
-        verbose = True
-    hosturl = sys.argv[1]
-    user = sys.argv[2]
-    password = sys.argv[3]
-  else:
-    # we're dealing with new-style parameters, so go get them!
-    (options, args) = parser.parse_args()
-
-    # Making sure all mandatory options appeared.
-    mandatories = ['host', 'user', 'password']
-    for m in mandatories:
-      if not options.__dict__[m]:
-        print "mandatory parameter '--" + m + "' is missing\n"
-        parser.print_help()
-        sys.exit(-1)
 
     hostname=options.host.lower()
     # if user has put "https://" in front of hostname out of habit, do the right thing
@@ -105,6 +104,8 @@ def getopts() :
     user=options.user
     password=options.password
     verbose=options.verbose
+    summary=options.summary
+    clean=options.clean
 
 
 # ----------------------------------------------------------------------
@@ -113,9 +114,14 @@ getopts()
 
 # ----------------------------------------------------------------------
 
-def verboseoutput(message) :
-  if verbose:
-    print bcolors.BLUE + '[*]' + bcolors.ENDC, "%s %s" % (time.strftime("%Y%m%d %H:%M:%S"), message)
+def coloroutput(message) :
+    msg = ''
+    for m in message:
+        msg += m + ' '
+    if clean:
+        print msg
+    else:
+        print bcolors.BLUE + '[*]' + bcolors.ENDC, "%s %s" % (time.strftime("%Y%m%d %H:%M:%S"), msg)
 
 # ----------------------------------------------------------------------
 
@@ -131,20 +137,18 @@ def get_all(host, name):
         print bcolors.YELLOW + '    [-] ', bcolors.ENDC, c, counters[c]
         print bcolors.GREEN + '        [--] ', bcolors.ENDC, pm.get_entity_statistic(host, counters[c]), bcolors.ENDC
 
-def get_property(mor, name):
-    prop = VIProperty(s, mor)
-    print bcolors.RED + '[+]' + bcolors.ENDC, "Stats for", name
-    print bcolors.GREEN + '  [-]', bcolors.ENDC, "Overall Processor Usage:", prop.summary.quickStats.overallCpuUsage
-    print bcolors.GREEN + '  [-]', bcolors.ENDC, "Overall Memory Usage:", prop.summary.quickStats.overallMemoryUsage
-    print bcolors.GREEN + '  [-]', bcolors.ENDC, "Distributed Memory Fairness:", prop.summary.quickStats.distributedMemoryFairness
-    print bcolors.GREEN + '  [-]', bcolors.ENDC, "Distributed Cpu Fairness:", prop.summary.quickStats.distributedCpuFairness
-    #the info in prop is cached when constructed, to flush the cache
-    prop._flush_cache()
+def get_metrics(host, name):
+	counters = pm.get_entity_counters(VIMor(host, MORTypes.HostSystem))
+	for m in metrics:
+		ms = pm.get_entity_statistic(host, counters[m])[0]
+		coloroutput([m, ms.value, ms.unit])
 
 for host, name in s.get_hosts().items():
     if verbose:
         get_all(host, name)
-    else:
-        get_property(host, name)
+    # else:
+    #     get_property(host, name)
+    if summary:
+    	get_metrics(host, name)
 
 s.disconnect()
